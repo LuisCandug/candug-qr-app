249fd1cb996208caeb38c0274234e56a8634f2b5
martinjosephbasilio <martinjosephbasilio012@gmail.com>
2025-06-20 10:36:51 +0800

    added new vue file

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,11 +1,5 @@
 <template>
-    <div>
-      <v-img
-        class="mx-auto my-6"
-        max-width="228"
-        src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-      ></v-img>
-  
+    <div class = "d-flex justify-center align-center" style="height: 100vh;">
       <v-card
         class="mx-auto pa-12 pb-8"
         elevation="8"
@@ -81,4 +75,10 @@
 import { ref } from 'vue'
 
 const visible = ref(false)
+</script>
+
+<script>
+export default {
+  layout: "auth",
+}
 </script>
